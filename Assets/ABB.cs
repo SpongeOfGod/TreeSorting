553f12715ae575b0b7385dc33b7a769b5fe0cd73
SpongeOfGod <157647080+SpongeOfGod@users.noreply.github.com>
2024-10-11 16:35:15 -0300

    Spawneo Hecho

--- Assets/ABB.cs
+++ Assets/ABB.cs
@@ -9,6 +9,11 @@ public class ABB : MonoBehaviour
     [SerializeField] Queue<Nodo> nodos = new Queue<Nodo>();
     bool rootAssigned = false;
     Nodo rootNodo;
+    [SerializeField] float posX, posXm;
+    [SerializeField] float posY;
+    [SerializeField] int depth = 0;
+    [SerializeField] float offsetMultiplier;
+
     private void Awake()
     {
 
@@ -19,57 +24,55 @@ public class ABB : MonoBehaviour
         {
             if (rootAssigned) 
             {
-                SearchInside(dataList[i], rootNodo, rootNodo.transform);
+                SearchInside(dataList[i], rootNodo);
+                depth = 0;
             }
             else 
             {
                 rootAssigned = true;
                 Nodo root = Instantiate(NodoPrefab, transform);
-                root.AssignData(dataList[0], null, null);
+                root.AssignData(dataList[0], null, null, null);
                 nodos.Enqueue(root);
                 rootNodo = root;
             }
         }
     }
 
-    void Update()
-    {
-        
-    }
-
-
-    private void SearchInside(int data, Nodo nodo, Transform parent) 
+    private void SearchInside(int data, Nodo nodo) 
     {
         if(data < nodo.dato) 
         {
+            Debug.Log($"{data} < {nodo.dato}");
             if (nodo.izq == null) 
             {
-                Debug.Log(parent);
-                nodo.izq = Instantiate(NodoPrefab, parent);
-                nodo.izq.AssignData(data, null, null);
-                nodo.parent = parent;
+                nodo.izq = Instantiate(NodoPrefab, nodo.transform);
+                nodo.izq.AssignData(data, null, null, nodo);
+                nodo.izq.GetComponent<RectTransform>().anchoredPosition = new Vector2(posX * (offsetMultiplier/ (depth + 1)), posY);
+                ////nodo.izq.transform.position = new Vector2(posX - depth, posY);
                 return;
             }
             else
             {
-                SearchInside(data, nodo.izq, nodo.izq.transform);
+                depth++;
+                SearchInside(data, nodo.izq);
                 return;
             }        
         }
 
         if (data > nodo.dato)
         {
+            Debug.Log($"{data} > {nodo.dato}");
             if (nodo.der == null)
             {
-                Debug.Log(parent);
-                nodo.der = Instantiate(NodoPrefab, parent);
-                nodo.der.AssignData(data, null, null);
-                nodo.parent = parent;
+                nodo.der = Instantiate(NodoPrefab, nodo.transform);
+                nodo.der.AssignData(data, null, null, nodo);
+                nodo.der.GetComponent<RectTransform>().anchoredPosition = new Vector2(posXm * (offsetMultiplier / (depth + 1)), posY);
                 return;
             }
             else
             {
-                SearchInside(data, nodo.der, nodo.der.transform);
+                depth++;
+                SearchInside(data, nodo.der);
                 return;
             }
         }
--- Assets/Nodo.cs
+++ Assets/Nodo.cs
@@ -7,7 +7,7 @@ public class Nodo : MonoBehaviour
 {
     public Nodo izq, der;
     public int dato;
-    public Transform parent;
+    public Nodo parent;
     [SerializeField] private TextMeshProUGUI dataText;
     private void Start()
     {
@@ -18,13 +18,15 @@ public class Nodo : MonoBehaviour
         this.izq = izq;
         this.der = der;
         this.dato = dato;
+        parent = null;
     }
 
-    public void AssignData(int dato, Nodo izq, Nodo der) 
+    public void AssignData(int dato, Nodo izq, Nodo der, Nodo parent) 
     {
         this.izq = izq;
         this.der = der;
         this.dato = dato;
+        this.parent = parent;
         dataText.text = dato.ToString();
     }
 }
