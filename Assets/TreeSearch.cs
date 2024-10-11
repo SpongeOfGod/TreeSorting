1b67d1ac8bee994a99415f299e429d5d6403cd7f
SpongeOfGod <157647080+SpongeOfGod@users.noreply.github.com>
2024-10-11 17:57:47 -0300

    InOrders fans cuando llegan los post ordenadores
    Ojala haya un sorting tree para mis neuronas.

--- Assets/TreeSearch.cs
+++ Assets/TreeSearch.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,23 +8,60 @@ public class TreeSearch : MonoBehaviour
 {
     public TreeSpawning treeSpawning;
     public Button preOrderButton;
-
+    public Button inOrderButton;
+    public Button postOrderButton;
+    public TextMeshProUGUI dataDisplay;
     private void Awake()
     {
         preOrderButton.onClick.AddListener(SearchByPreOrder);
+        inOrderButton.onClick.AddListener(SearchByInOrder);
+        postOrderButton.onClick.AddListener(SearchByPostOrder);
     }
 
     void SearchByPreOrder() 
     {
-        checkPreOrder(treeSpawning.RootNodo);
+        dataDisplay.text = string.Empty;
+        CheckPreOrder(treeSpawning.RootNodo);
+        dataDisplay.text.Remove(dataDisplay.text.Length - 1);
     }
 
-    void checkPreOrder(Nodo nodo) 
+    void SearchByInOrder() 
+    {
+        dataDisplay.text = string.Empty;
+        CheckInOrder(treeSpawning.RootNodo);
+        dataDisplay.text.Remove(dataDisplay.text.Length - 1);
+    }
+
+    void SearchByPostOrder() 
+    {
+        dataDisplay.text = string.Empty;
+        CheckPostOrder(treeSpawning.RootNodo);
+        dataDisplay.text.Remove(dataDisplay.text.Length - 1);
+    }
+
+    void CheckPreOrder(Nodo nodo) 
     {
         if (nodo == null) return;
 
-        Debug.Log(nodo.dato);
-        checkPreOrder(nodo.izq);
-        checkPreOrder(nodo.der);
+        dataDisplay.text += $" {nodo.dato},";
+        CheckPreOrder(nodo.izq);
+        CheckPreOrder(nodo.der);
     }
+
+    void CheckInOrder(Nodo nodo) 
+    {
+        if (nodo == null) return;
+
+        CheckInOrder(nodo.izq);
+        dataDisplay.text += $" {nodo.dato},";
+        CheckInOrder(nodo.der);
+    }
+    void CheckPostOrder(Nodo nodo) 
+    {
+        if (nodo == null) return;
+        CheckPostOrder(nodo.izq);
+        CheckPostOrder(nodo.der);
+        dataDisplay.text += $" {nodo.dato},";
+    }
+
 }
