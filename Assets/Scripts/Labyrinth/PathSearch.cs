232464df46ee0757c9c50775f0cc0638ff2586ef
littleusuario <alejo.marandola@gmail.com>
2024-11-12 00:51:20 -0300

    BFS (BEST FRIENDS SHIT) y super mega bugfixz

--- Assets/Scripts/Labyrinth/PathSearch.cs
+++ Assets/Scripts/Labyrinth/PathSearch.cs
@@ -10,17 +10,19 @@ public class PathSearch : MonoBehaviour
     bool once;
     [SerializeField] List<VisualVertice> verticesPath;
     int currentIndex = 0;
+
     public void RunUpdate()
     {
         if (graphManager.PlayerVertice != null && !once)
         {
             once = true; // Inicia el chequeo de aristas salientes, aristas que tienen como origen al nodo especifico.
             verticesPath = CheckVerticeSaliente(graphManager.PlayerVertice.Vertice, new List<VisualVertice>());
-        }                // Se utiliza Deep First Search, avanzando lo más posible hasta llegar a un bloqueo.
-                         // Al chocar con el bloqueo (un nodo vacio o un nodo que ya se ha visitado), se retrocede hasta un nodo
-                         // Que tenga caminos por explorar.
+        }                // Se utiliza Breadth First Search (BFS).
 
-        TravelPath(verticesPath);
+        if (verticesPath != null && verticesPath.Count > 0)
+        {
+            TravelPath(verticesPath);
+        }
     }
 
     public void TravelPath(List<VisualVertice> verticesPath)
@@ -46,70 +48,55 @@ public class PathSearch : MonoBehaviour
     public List<VisualVertice> CheckVerticeSaliente(Vertice vertice, List<VisualVertice> verticesPath)
     {
         if (graphManager.ExitVertice == null) return null;
-        List<VisualVertice> currentPath = new List<VisualVertice>();
 
-        if (verticesPath.Contains(graphManager.ExitVertice.Vertice.VerticeVisual) && !graphManager.Labyrinth)
+        // Usamos una cola para BFS
+        Queue<Vertice> queue = new Queue<Vertice>();
+        Dictionary<Vertice, List<VisualVertice>> paths = new Dictionary<Vertice, List<VisualVertice>>(); // Para rastrear el camino recorrido
+
+        // Agregamos el vértice inicial a la cola
+        queue.Enqueue(vertice);
+        paths[vertice] = new List<VisualVertice> { vertice.VerticeVisual };
+
+        while (queue.Count > 0)
         {
-            return verticesPath;
-        }
+            var currentVertice = queue.Dequeue();
 
-        if (vertice.visited) // Si se ha visitado el nodo, se lo remueve del camino
-        {
-            if (verticesPath.Count > 0)
-                verticesPath.RemoveAt(verticesPath.Count - 1);
-            return verticesPath;
-        }
-
-        if (graphManager.Labyrinth) 
-        {
-            vertice.visited = true;
-        }
-        currentPath.Add(vertice.VerticeVisual);
-
-        VisualVertice currentVert = vertice.VerticeVisual;
-        graphManager.Graph.adyacentList.TryGetValue(vertice, out List<(Vertice, Arista)> AristasSalientes);
-
-        List<List<VisualVertice>> Paths = new List<List<VisualVertice>>();
-        if (AristasSalientes.Count > 0) // Si tiene más de una arista que lleve a otro nodo
-        {
-            for (int i = 0; i < AristasSalientes.Count; i++) // Se comprueba que no sea un nodo ya visitado
+            // Si encontramos el vértice de salida, devolvemos el camino
+            if (currentVertice == graphManager.ExitVertice.Vertice)
             {
-                if (!AristasSalientes[i].Item2.DestinationVert.visited && !verticesPath.Contains(graphManager.ExitVertice.Vertice.VerticeVisual))
-                {
-                    currentVert = AristasSalientes[i].Item2.DestinationVert.VerticeVisual;
-                    List<VisualVertice> vertices = new List<VisualVertice>();
-                    currentPath.Clear();
-                    currentPath.Add(vertice.VerticeVisual);
-                    vertices = currentPath;
-                    vertices.AddRange(verticesPath);
-                    vertices = CheckVerticeSaliente(currentVert.Vertice, vertices);
-                    Paths.Add(vertices); // Recursividad - Crea una lista partir del vertice elegido.
-                }
+
+                var finalPath = paths[currentVertice];
+
+                if (!graphManager.Labyrinth)
+                    finalPath.Reverse();
+
+                return finalPath;
             }
 
-            if (Paths.Count > 0) 
+            // Recorremos los vecinos (aristas salientes)
+            var adjacentEdges = graphManager.Graph.adyacentList.GetValueOrDefault(currentVertice, new List<(Vertice, Arista)>());
+
+            foreach (var edge in adjacentEdges)
             {
-                currentPath = Paths[0];
-                foreach (var item in Paths)
+                Vertice neighbor = edge.Item2.DestinationVert;
+
+                // Si el vecino no ha sido visitado
+                if (!paths.ContainsKey(neighbor) && !neighbor.visited)
                 {
-                    if (currentPath.Count > item.Count && item.Contains(graphManager.ExitVertice)) 
-                    {
-                        currentPath = item;
-                    }
+                    // Marcamos el vecino como visitado (para evitar ciclos)
+                    neighbor.visited = true;
+
+                    // Creamos un nuevo camino añadiendo el vértice vecino
+                    List<VisualVertice> newPath = new List<VisualVertice>(paths[currentVertice]) { neighbor.VerticeVisual };
+                    paths[neighbor] = newPath;
+
+                    // Agregamos el vecino a la cola para explorarlo
+                    queue.Enqueue(neighbor);
                 }
             }
         }
 
-        if (vertice == graphManager.ExitVertice.Vertice)
-        {
-            currentPath.AddRange(verticesPath);
-            return currentPath;
-        }
-        else
-        {
-            vertice.visited = true;
-        }
-
-        return CheckVerticeSaliente(currentVert.Vertice, currentPath); // Cuando se llega a un camino sin saluda, se regresa.
+        // Si no se encontró un camino, devolvemos null
+        return null;
     }
 }
--- Assets/Scripts/Tree/TreeABB.cs
+++ Assets/Scripts/Tree/TreeABB.cs
@@ -1,46 +1,51 @@
 ﻿[System.Serializable]
-    public class TreeABB : TreeStructure 
+public class TreeABB : TreeStructure
+{
+    public void Insert(int value)
     {
-        public void Insert(int value) 
+        root = InsertNode(root, value);
+    }
+
+    public override Nodo InsertNode(Nodo node, int value)
+    {
+        if (node == null)
         {
-            root = InsertNode(root, value);
+            return new Nodo(value);
         }
 
-        public override Nodo InsertNode(Nodo node, int value)
+        if (value < node.dato)
         {
-            if (node == null) 
-            {
-                return new Nodo(value);
-            }
-            if (value < node.dato) 
-            {
-                depth++;
-                node.izq = InsertNode(node.izq, value);
-                node.izq.visualNode.name = "izq";
-                node.izq.visualNode.ParentNode = node.dato.ToString();
-                node.izq.visualNode.transform.SetParent(node.visualNode.transform, true);
-                node.izq.setParentNode(node);
-                node.izq.positionX = -posX + offsetMultiplier * node.depth;
-                node.izq.visualNode.SetVisualPosition(posY, offsetMultiplier);
-                depth = 0;
-            } 
-            else if (value > node.dato) 
-            {
-                depth++;
-                node.der = InsertNode(node.der, value);
-                node.der.visualNode.name = "der";
-                node.der.visualNode.ParentNode = node.dato.ToString();
-                node.der.visualNode.transform.SetParent(node.visualNode.transform, true);
-                node.der.setParentNode(node);
-                node.der.positionX = posX - offsetMultiplier * node.depth;
-                node.der.visualNode.SetVisualPosition(posY, offsetMultiplier);
-                depth = 0;
-            }
-            else
-            {
-                return node;
-            }
+            depth++;
+            node.izq = InsertNode(node.izq, value);
 
+            node.izq.visualNode.name = $"{node.izq.dato} (Izquierda)";
+            node.izq.visualNode.ParentNode = node.dato.ToString();
+            node.izq.visualNode.transform.SetParent(node.visualNode.transform, true);
+            node.izq.setParentNode(node);
+
+            node.izq.positionX = -posX + offsetMultiplier * node.depth;
+            node.izq.visualNode.SetVisualPosition(posY, offsetMultiplier);
+            depth = 0;
+        }
+        else if (value > node.dato)
+        {
+            depth++;
+            node.der = InsertNode(node.der, value);
+
+            node.der.visualNode.name = $"{node.der.dato} (Derecha)";
+            node.der.visualNode.ParentNode = node.dato.ToString();
+            node.der.visualNode.transform.SetParent(node.visualNode.transform, true);
+            node.der.setParentNode(node);
+
+            node.der.positionX = posX - offsetMultiplier * node.depth;
+            node.der.visualNode.SetVisualPosition(posY, offsetMultiplier);
+            depth = 0;
+        }
+        else
+        {
             return node;
         }
+
+        return node;
     }
+}
